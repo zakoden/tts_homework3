e17ffd7c452ad6db37a84e337b2b998df18e3537
zakoden <59803738+zakoden@users.noreply.github.com>
2022-11-27 03:36:56 +0300

    Create buffer_dataset.py

---
+++ datasets/buffer_dataset.py
@@ -0,0 +1,10 @@
+class BufferDataset(Dataset):
+    def __init__(self, buffer):
+        self.buffer = buffer
+        self.length_dataset = len(self.buffer)
+
+    def __len__(self):
+        return self.length_dataset
+
+    def __getitem__(self, idx):
+        return self.buffer[idx]
