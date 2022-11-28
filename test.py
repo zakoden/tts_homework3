71e56ce37984ef9714abfbc5bf4445edfb0b5b25
zakoden <59803738+zakoden@users.noreply.github.com>
2022-11-28 19:19:02 +0300

    Update test.py

--- test.py
+++ test.py
@@ -13,6 +13,7 @@ from torch import nn
 
 from torch.utils.data import DataLoader
 from torch.optim.lr_scheduler  import OneCycleLR
+import wandb
 
 import waveglow
 import text
@@ -37,19 +38,16 @@ torch.backends.cudnn.benchmark = False
 np.random.seed(SEED)
 
 def main(args):
-    buffer, variance_stats = datasets.get_data_to_buffer(train_config)
-    dataset = datasets.BufferDataset(buffer)
-    
-    training_loader = DataLoader(
-        dataset,
-        batch_size=train_config.batch_expand_size * train_config.batch_size,
-        shuffle=True,
-        collate_fn=dataloader.collate_fn_tensor,
-        drop_last=True,
-        num_workers=0
-    )
-    
+    variance_stats = {
+      'pitch_min': 0.0,
+      'pitch_max': 861.0652680130653,
+      'energy_min': 0.013677556999027729,
+      'energy_max': 246.5960235595703
+    }
+
     model = FastSpeech(model_config, variance_stats)
+    model_weights = (torch.load(args.checkpoint))['model']
+    model.load_state_dict(model_weights)
     model = model.to(train_config.device)
     
     WaveGlow = utils.get_WaveGlow()
@@ -66,10 +64,10 @@ def main(args):
     f = open(args.input, "r")
     for line in f:
       line_lst = line.split(" ")
-      alpha_d = int(line_lst[0])
-      alpha_p = int(line_lst[1])
-      alpha_e = int(line_lst[2])
-      cur_text = str(line_lst[-1])
+      alpha_d = float(line_lst[0])
+      alpha_p = float(line_lst[1])
+      alpha_e = float(line_lst[2])
+      cur_text = str(" ".join(line_lst[3:]))[:-1]
       log_data.append([cur_text, logger.step, alpha_d, alpha_p, alpha_e, None])
       tests.append(cur_text)
     f.close()
@@ -77,11 +75,13 @@ def main(args):
     data_list = list(text.text_to_sequence(test, train_config.text_cleaners) for test in tests)
     
     for i, phn in enumerate(data_list):
-      alpha_d = log_data[i][1]
-      alpha_p = log_data[i][2]
-      alpha_e = log_data[i][3]
-      mel_cuda = one_synthesis(model, phn, alpha_duration=alpha_d, alpha_pitch=alpha_p, alpha_energy=alpha_e)
-      cur_path = f"results/d=({alpha_d})_p=({alpha_p})_e=({alpha_e})_{i}_waveglow.wav"
+      alpha_d = log_data[i][2]
+      alpha_p = log_data[i][3]
+      alpha_e = log_data[i][4]
+      print("|" + tests[i] + "|")
+      print("alpha_duration=", alpha_d, "alpha_pitch=", alpha_p, "alpha_energy=", alpha_e)
+      mel_cuda = synthesis.one_synthesis(model, phn, alpha_duration=alpha_d, alpha_pitch=alpha_p, alpha_energy=alpha_e)
+      cur_path = args.output + f"/d=({alpha_d})_p=({alpha_p})_e=({alpha_e})_{i}_waveglow.wav"
       waveglow.inference.inference(mel_cuda, WaveGlow, cur_path)
       cur_audio = wandb.Audio(cur_path)
       log_data[i][-1] = cur_audio
@@ -91,6 +91,14 @@ def main(args):
 
 
 if __name__ == "__main__":
+    args = argparse.ArgumentParser(description="test TTS")
+    args.add_argument(
+        "-c",
+        "--checkpoint",
+        default="checkpoint",
+        type=str,
+        help="Path to model checkpoint.",
+    )
     args.add_argument(
         "-i",
         "--input",
